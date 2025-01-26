3bec1ec3bbfd5a5e9d83916860842a88180d32c5
luizffdemoraes <lffm1994@gmail.com>
2025-01-25 23:12:23 -0300

    Implementando o checklist do Spring Security PARTE 1

--- password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/User.java
+++ password-grant/spring-boot-3-1-0-noauth/src/main/java/com/devsuperior/demo/entities/User.java
@@ -1,15 +1,19 @@
 package com.devsuperior.demo.entities;
 
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
-import jakarta.persistence.*;
-
+@SuppressWarnings("serial")
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -70,6 +74,36 @@ public class User {
 
     public void addRole(Role role) {
         this.roles.add(role);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public boolean hasRole(String roleName) {
